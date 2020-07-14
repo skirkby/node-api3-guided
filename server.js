9ef2c39af5dcba12160e7f04fa4586649db90628
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-07-13 22:35:30 -0400

    working on middleware guided project

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express'); // importing a CommonJS module
 
 const hubsRouter = require('./hubs/hubs-router.js');
-
+const helmet = require('helmet');
+const morgan = require('morgan');
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
+server.use(helmet());
+// server.use(morgan('dev'));
+server.use(methodLogger);
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
@@ -16,5 +20,15 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+server.delete('/', (req,res) => {
+  res.send('deleted');
+});
+
+function methodLogger(req, res, next) {
+  console.log(`${req.method} request`);
+  next();
+}
+
 
 module.exports = server;
